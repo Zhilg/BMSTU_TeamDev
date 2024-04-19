aa32892366ad14eeeeea7d3b55b1bf3e05ac840e
Ahmad Khalid Karimzai <ak.karimzai1@mail.ru>
2024-04-19 23:29:54 +0300

    #3: убрал лешние комментари и добавил документации

--- lms/managers/managers.py
+++ lms/managers/managers.py
@@ -4,24 +4,54 @@ from bd_course.BaseManag import BaseUserManag, BaseManag
 from lms.exceptions import *
 from django.contrib.auth.models import BaseUserManager
 
-# для менеджеров свои модельки, а репозиторий конвертирует из модели в бд в модель для менеджеров
-# модели в ОРМ слишком сильно связаны с менеджерами, ослабить эту связь, абстрагироваться
-#создавать менеджеров через какойто конструктор, отдельный класс (фабрику), туда передавать связь с репозиторием
-
-
 class UserProfilesManager(BaseUserManag, BaseUserManager):
+    """
+    Менеджер профилей пользователей, обеспечивающий операции создания, получения и обновления пользовательских профилей.
+    """
+
     def __init__(self):
+        """
+        Инициализация менеджера профилей пользователей.
+        """
         super().__init__()
         self.rep = None
-        
+
     def create(self, form, **extra_fields):
- 
+        """
+        Создает новый пользовательский профиль на основе данных из формы.
+
+        Параметры:
+        - form (dict): Словарь с данными формы для создания профиля.
+        - extra_fields: Дополнительные поля профиля.
+
+        Возвращает:
+        - Созданный пользовательский профиль.
+        
+        Исключения:
+        - ValueError: В случае, если пользователь с указанным адресом электронной почты уже существует.
+        """
         if self.get(email=form['email']):
             raise ValueError
         user = self.rep.insert(form, **extra_fields)
         return user
 
-    def create_superuser(self, email, username, grup, password=None, **extra_fields): # зависимость передавать через конструктор
+    def create_superuser(self, email, username, grup, password=None, **extra_fields):
+        """
+        Создает нового суперпользователя с правами администратора.
+
+        Параметры:
+        - email (str): Адрес электронной почты суперпользователя.
+        - username (str): Имя пользователя суперпользователя.
+        - grup: Группа пользователя.
+        - password (str): Пароль для суперпользователя.
+        - extra_fields: Дополнительные поля профиля суперпользователя.
+
+        Возвращает:
+        - Созданный суперпользователь.
+        
+        Исключения:
+        - ValueError: Если не указаны обязательные параметры для создания суперпользователя.
+        """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -33,6 +63,19 @@ class UserProfilesManager(BaseUserManag, BaseUserManager):
         return self.rep.insert(email, username, grup, password, **extra_fields)
     
     def get(self, id=None, email=None):
+        """
+        Получает пользователя по его идентификатору или адресу электронной почты.
+
+        Параметры:
+        - id (int): Идентификатор пользователя.
+        - email (str): Адрес электронной почты пользователя.
+
+        Возвращает:
+        - Найденный пользователь.
+        
+        Исключения:
+        - DoesNotExist: Если пользователь с указанными параметрами не найден.
+        """
         if id:
             try:
                 return self.rep.get(user_id=id)
@@ -47,10 +90,25 @@ class UserProfilesManager(BaseUserManag, BaseUserManager):
         return self.rep.get()
         
     def auth_user(self, email, password, request):
+        """
+        Аутентифицирует пользователя на основе предоставленных адреса электронной почты и пароля.
+
+        Параметры:
+        - email (str): Адрес электронной почты пользователя.
+        - password (str): Пароль пользователя.
+        - request: Объект запроса.
+
+        Возвращает:
+        - Аутентифицированного пользователя.
+        
+        Исключения:
+        - ValueError: Если пользователь с указанным адресом электронной почты не найден или пароль неверен.
+        - KeyError: Если пользователь неактивен.
+        """
         email = self.normalize_email(email)
         user = self.get(email=email)[0]
         if user is None:
-            return ValueError
+            raise ValueError
         if not user.is_active:
             raise KeyError
         if user.check_password(password):
@@ -60,7 +118,27 @@ class UserProfilesManager(BaseUserManag, BaseUserManager):
             raise ValueError
     
     def update(self, instance, validated_data):
+        """
+        Обновляет данные профиля пользователя.
+
+        Параметры:
+        - instance: Экземпляр профиля пользователя.
+        - validated_data: Проверенные данные для обновления.
+
+        Возвращает:
+        - Обновленный профиль пользователя.
+        """
         return self.rep.update(instance, validated_data)
         
     def change_password(self, instance, validated_data):
+        """
+        Изменяет пароль пользователя.
+
+        Параметры:
+        - instance: Экземпляр профиля пользователя.
+        - validated_data: Проверенные данные для изменения пароля.
+
+        Возвращает:
+        - Обновленный профиль пользователя с измененным паролем.
+        """
         return self.rep.update_password(instance, validated_data)
--- lms/models/models.py
+++ lms/models/models.py
@@ -9,33 +9,97 @@ from django.contrib.auth import login
 # Create your models here.
 
 class UserProfiles(AbstractBaseUser):
-    last_login = models.DateTimeField(auto_now=True)
-    email = models.EmailField(unique=True)
-    username = models.CharField(max_length=100)
-    grup = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_authenticated = models.BooleanField(default=False)
+    """
+    Модель для хранения информации о пользователях.
+
+    Attributes:
+        last_login (DateTimeField): Дата и время последнего входа пользователя (автоматически обновляется).
+        email (EmailField): Электронная почта пользователя (уникальное значение).
+        username (CharField): Имя пользователя.
+        grup (CharField): Группа пользователя.
+        is_active (BooleanField): Определяет, активен ли пользователь (по умолчанию True).
+        is_staff (BooleanField): Определяет, является ли пользователь сотрудником (по умолчанию False).
+        is_authenticated (BooleanField): Определяет, аутентифицирован ли пользователь (по умолчанию False).
+
+    """
 
     USERNAME_FIELD = 'email'
     
     def set_password(self, raw_password):
+        """
+        Метод для установки пароля пользователя.
+
+        Args:
+            raw_password (str): Необработанный пароль пользователя.
+
+        """
         self.password = make_password(raw_password)
 
     def check_password(self, raw_password):
+        """
+        Метод для проверки пароля пользователя.
+
+        Args:
+            raw_password (str): Необработанный пароль, который нужно проверить.
+
+        Returns:
+            bool: True, если пароль верный, иначе False.
+
+        """
         return check_password(raw_password, self.password)
     
     def has_perm(self, perm, obj=None):
+        """
+        Метод для определения, имеет ли пользователь указанные разрешения.
+
+        Args:
+            perm (str): Разрешение, которое нужно проверить.
+            obj (object): Объект, для которого нужно проверить разрешение (по умолчанию None).
+
+        Returns:
+            bool: True, если пользователь имеет указанное разрешение, иначе False.
+
+        """
         return self.is_staff
 
     def has_module_perms(self, app_label):
+        """
+        Метод для определения, имеет ли пользователь разрешения для заданного модуля.
+
+        Args:
+            app_label (str): Метка приложения.
+
+        Returns:
+            bool: True, если пользователь имеет разрешения для модуля, иначе False.
+
+        """
         return self.is_staff    
 
 class Tasks(models.Model):
+    """
+    Модель для хранения информации о заданиях.
+
+    Attributes:
+        filename (CharField): Имя файла задания (уникальное значение).
+        theme (CharField): Тема задания.
+
+    """
     filename = models.CharField(max_length=100, unique=True)
     theme = models.CharField(max_length=100)
             
 class TaskPacks(models.Model):
+    """
+    Модель для хранения информации о пакетах заданий.
+
+    Attributes:
+        tasks (ManyToManyField): Связь с моделью Tasks.
+        student (ForeignKey): Ссылка на студента, связанного с этим пакетом заданий.
+        teacher (ForeignKey): Ссылка на учителя, связанного с этим пакетом заданий.
+        maxgrade (IntegerField): Максимальная оценка, которую может получить студент за выполнение пакета заданий.
+        mingrade (IntegerField): Минимальная оценка, которую может получить студент за выполнение пакета заданий.
+        duetime (DateField): Крайний срок выполнения пакета заданий.
+
+    """
     tasks = models.ManyToManyField(Tasks)
     student = models.ForeignKey(UserProfiles, on_delete=models.CASCADE, related_name='student_taskpacks')
     teacher = models.ForeignKey(UserProfiles, on_delete=models.CASCADE, related_name='teacher_taskpacks')
@@ -44,6 +108,18 @@ class TaskPacks(models.Model):
     duetime = models.DateField()
 
 class Solutions(models.Model):
+    """
+    Модель для хранения информации о решениях заданий.
+
+    Attributes:
+        student (ForeignKey): Ссылка на студента, связанного с этим решением.
+        teacher (ForeignKey): Ссылка на учителя, связанного с этим решением.
+        taskpack (ForeignKey): Ссылка на пакет заданий, к которому относится это решение.
+        grade (IntegerField): Оценка за решение (по умолчанию -1).
+        filename (CharField): Имя файла решения.
+        sendtime (DateField): Дата и время отправки решения (автоматически обновляется).
+
+    """
     student = models.ForeignKey(UserProfiles, on_delete=models.CASCADE, related_name='student_solutions')
     teacher = models.ForeignKey(UserProfiles, on_delete=models.CASCADE, related_name='teacher_solutions')
     taskpack = models.ForeignKey(TaskPacks, on_delete=models.CASCADE)
