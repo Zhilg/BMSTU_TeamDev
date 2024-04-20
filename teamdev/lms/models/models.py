ebba47d8335c10108164bdd3d2deed6f6ed00393
Zhilg <denisyakovlev02@yandex.ru>
2024-04-21 00:43:37 +0300

    Реализована базовая модель для профиля пользователя

--- teamdev/lms/models/models.py
+++ teamdev/lms/models/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.hashers import make_password, check_password
+
 class Tasks(models.Model):
     filename = models.CharField(max_length=100, unique=True)
     theme = models.CharField(max_length=100)
+
+class UserProfiles(AbstractBaseUser):
+    last_login = models.DateTimeField(auto_now=True)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100)
+    grup = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_authenticated = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
+
+    def has_module_perms(self, app_label):
+        return self.is_staff
