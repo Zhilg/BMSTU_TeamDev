69bbfb60fe5a4615a2e730f9533da56ca084c4b1
Alex <al_mezh@mail.ru>
2024-04-22 00:11:14 +0300

    #44 Добавил view для решений

--- teamdev/lms/views/views.py
+++ teamdev/lms/views/views.py
@@ -161,3 +161,111 @@ class SingleTaskPackView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SolutionsView(APIView):
+    @swagger_auto_schema(responses={201: "created", 400: "bad request", 401: "unauthorized", 500: 'failed'},
+                         request_body=openapi.Schema
+                             (
+                             type=openapi.TYPE_OBJECT,
+                             properties={
+                                 "taskpackid": openapi.Schema(
+                                     type=openapi.TYPE_INTEGER,
+                                     example=1
+                                 ),
+                                 "filename": openapi.Schema(
+                                     type=openapi.TYPE_STRING,
+                                     example='solution1000.txt'
+                                 ),
+                             },
+
+                             required=["taskpackid", "filename"]
+                         ))
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = SolutionsSerializer(data=request.data, context={"user": request.user})
+        if serializer.is_valid():
+            try:
+                sol = serializer.save()
+                response_data = {'message': 'Successful operation'}
+                response_headers = {'Location': f'http://localhost:8000/solutions/{sol.id}'}
+                return Response(status=status.HTTP_201_CREATED, data=response_data, headers=response_headers)
+            except FileAlreadyExists:
+                return Response({"message": "Файл с таким названием уже существует в системе"},
+                                status=status.HTTP_400_BAD_REQUEST)
+            except NoSuchTaskPacks:
+                return Response({"message": "Данного комплекта заданий не существует"},
+                                status=status.HTTP_404_NOT_FOUND)
+            except WrongTaskPackID:
+                return Response({"message": "Данный комплект заданий не ваш"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(responses={200: "success", 401: "unauthorized", 500: 'failed'})
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        solutio = SM.get()
+        serializer = SolutionsModelSerializer(solutio, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SingleSolutionView(APIView):
+    @swagger_auto_schema(responses={200: "success", 401: "unauthorized", 404: "Not found", 500: 'failed'})
+    def get(self, request, id):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if id is None or id < 0:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        sol = SM.get(id=id)
+        if not sol:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SolutionsModelSerializer(sol[0])
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(responses={204: "success", 401: "unauthorized", 404: "Not found", 500: 'failed'})
+    def delete(self, request, id):
+
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if id is None or id < 0:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        sol = SM.get(id=id)
+        if sol is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        sol.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @swagger_auto_schema(
+        responses={201: "created", 400: "bad request", 401: "unauthorized", 403: "Not permitted", 500: 'failed'},
+        request_body=openapi.Schema
+            (
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "grade": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    example=10
+                )
+            },
+
+            required=["grade"]
+        ))
+    def patch(self, request, id):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if not request.user.is_staff and not request.user.grup != "Teacher":
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if id is None or id < 0:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        sol = SM.get(id=id)[0]
+        if not sol:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = SolutionsModelSerializer(sol, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.update()
+            return Response(serializer.validated_data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
