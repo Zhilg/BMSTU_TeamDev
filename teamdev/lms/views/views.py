bc10bbf0add268b709b29cd16352cf59e65faf75
Alex <al_mezh@mail.ru>
2024-04-22 00:03:24 +0300

    #43 Добавил view для комплектов задач

--- teamdev/lms/views/views.py
+++ teamdev/lms/views/views.py
@@ -56,3 +56,108 @@ class TasksView(APIView):
         tasks = TM.get()
         serializer = TasksSerializer(tasks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class TaskPacksView(APIView):
+    @swagger_auto_schema(
+        responses={201: "created", 400: "bad request", 401: "unauthorized", 403: "Not permitted", 500: 'failed'},
+        request_body=openapi.Schema
+            (
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "n": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    example=1
+                ),
+                "duetime": openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    example='2023-12-29'
+                ),
+                "theme": openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    example='maths'
+                ),
+                'group': openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    example='IU7-71B'
+                ),
+                "maxgrade": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    example=10
+                ),
+                "mingrade": openapi.Schema(
+                    type=openapi.TYPE_INTEGER,
+                    example=0
+                )
+            },
+
+            required=["n", "duetime", "theme", "group", "maxgrade", "mingrade"]
+        ))
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if not request.user.is_staff and not request.user.grup != "Teacher":
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        serializer = TaskPacksSerializer(data=request.data, partial=True, context={"user": request.user})
+        if serializer.is_valid():
+            try:
+                taskpack = serializer.save()
+
+                response_data = {'message': 'Successful operation'}
+                return Response(status=status.HTTP_201_CREATED, data=response_data)
+
+            except NonPositiveNException:
+                return Response({"message": 'Количество заданий в комплекте не может быть <= 0'},
+                                status=status.HTTP_400_BAD_REQUEST)
+            except WrongGrades:
+                return Response({"message": 'Неправильные границы оценки'}, status=status.HTTP_400_BAD_REQUEST)
+            except NoSuchGroupException:
+                return Response({"message": 'Такой группы нет в системе'}, status=status.HTTP_400_BAD_REQUEST)
+            except WrongDeadlineException:
+                return Response({"message": "Неправильный дедлайн данных комплектов"},
+                                status=status.HTTP_400_BAD_REQUEST)
+            except NoSuchThemeException:
+                return Response({"message": 'Заданий с такой темой нет в системе'}, status=status.HTTP_400_BAD_REQUEST)
+            except NotEnoughTasksException:
+                return Response({"message": 'Недостаточно заданий в системе для формирования комплекта'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(responses={200: "success", 401: "unauthorized", 500: 'failed'})
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        taskpacks = TPM.get()
+        serializer = TaskPacksModelSerializer(taskpacks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class SingleTaskPackView(APIView):
+    @swagger_auto_schema(responses={200: "success", 401: "unauthorized", 404: "Not found", 500: 'failed'})
+    def get(self, request, id):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if id is None or id < 0:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        taskpack = TPM.get(id=id)
+        if not taskpack:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = TaskPacksModelSerializer(taskpack[0])
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        responses={200: "success", 401: "unauthorized", 403: "Not permitted", 404: "Not found", 500: 'failed'})
+    def delete(self, request, id):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        if not request.user.is_staff and not request.user.grup != "Teacher":
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if id is None or id < 0:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        task = TPM.get(id=id)
+        if task is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
